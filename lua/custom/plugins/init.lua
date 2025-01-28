b2f5833abd5b294c580735c9e82a771fa6f130fc
Ömer <grousee@proton.me>
2025-01-28 22:46:01 +0300

    changed to lualine from mini.statusline, added autopairs and tinkered some options

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,26 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons', 'arkav/lualine-lsp-progress' },
+
+    opts = {
+      options = {
+        icons_enabled = true,
+        theme = 'auto',
+        component_separators = '',
+        section_separators = '',
+      },
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diff', 'diagnostics' },
+        lualine_c = {},
+        lualine_x = { 'lsp-progress', 'language-server', 'diagnostics' },
+        lualine_y = { 'filetype' },
+        lualine_z = { 'location', 'progress' },
+      },
+    },
+  },
+}
